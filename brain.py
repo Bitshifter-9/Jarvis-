eb476a6a790d116144e2b82f0dd206d1fe372d0c
Bitshifter-9 <bandarampranav@gmail.com>
2026-02-17 10:25:41 +0530

    ollama llama3 implemented

---
+++ brain.py
@@ -0,0 +1,29 @@
+import ollama
+from rich import print
+
+
+System_prompt='''
+You are Jarvis, a smart, calm, and helpful AI assistant.
+Be concise, clear, and intelligent.
+'''
+
+def chat():
+    message = [{"role":"system","content":System_prompt}]
+    print("[bold green]Jarvis is online.Type exit to quite.[/bold green]")
+
+    while True:
+        user_input=input("\nYou:")
+
+        if user_input.lower()=="exit":
+            print("Jarvis Goodbye.")
+            break
+        message.append({"role":"user","content":user_input})
+        response=ollama.chat(
+            model = "llama3:8b-instruct-q4_K_M",
+            messages=message
+        )
+        reply=response["message"]["content"]
+        message.append({"role":"assistant","content":reply})
+        print(f"[cyan]Jarvis:[/cyan] {reply}")
+if __name__=="__main__":
+    chat()
